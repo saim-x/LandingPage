9b31d7e085d65a54d7a9f3eb138ccec25d2a1636
Abstract Programmer <AbstractProgrammer@example.com>
2023-11-16 20:42:52 +0500

    commit message here

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('.show-all-reviews').addEventListener('click', function () {
+    document.getElementById('reviews-screen').style.display = 'block';
+document.getElementById('reviews-screen').scrollIntoView({behavior: 'smooth' });
+});
